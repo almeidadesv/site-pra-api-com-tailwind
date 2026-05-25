d09d119fdc4185b070b32635b0e3d694f191ad1c
almeidadesv <alikaaalmeid@gmail.com>
2026-05-25 19:23:25 -0300

    corrigindo layout ruim

--- api.js
+++ api.js
@@ -2,71 +2,44 @@ let receitas = [];
 let index = 0;
 
 async function buscarReceitas() {
-  try {
-    let data = await fetch("https://dummyjson.com/recipes").then(r => r.json());
+try {
+let data = await fetch("https://dummyjson.com/recipes").then(r => r.json());
 
-    receitas = data.recipes;
+receitas = data.recipes;
 
-    mostrarReceita();
-
+  mostrarReceita();
   } catch (erro) {
-    console.log("Erro ao buscar receitas:", erro);
+  console.log("Erro ao buscar receitas:", erro);
   }
 }
 
 function mostrarReceita() {
-  try {
-    let receita = receitas[index];
+try {
+let receita = receitas[index];
 
-    document.getElementById("imagemdacomida").src = receita.image;
-
-    document.getElementById("nome").innerHTML =
-      "Nome da receita: " + receita.name;
-
-    document.getElementById("ingredientes").innerHTML =
-      "Ingredientes: " + receita.ingredients.join(", ");
-
-    document.getElementById("instrucoes").innerHTML =
-      "Instruções: " + receita.instructions;
-
-    document.getElementById("avaliacao").innerHTML =
-      "Avaliação: " + receita.rating;
+document.getElementById("imagemdacomida").src = receita.image;
+document.getElementById("nome").innerHTML = "Nome da receita: " + receita.name;
+document.getElementById("ingredientes").innerHTML = "Ingredientes: " + receita.ingredients;
+document.getElementById("instrucoes").innerHTML = "Instruções: " + receita.instructions;
+document.getElementById("avaliacao").innerHTML = "Avaliação: " + receita.rating;
 
   } catch (erro) {
-    console.log("Erro ao mostrar receita:", erro);
+  console.log("Erro ao mostrar a receita:", erro);
   }
 }
 
-let botoes = document.querySelectorAll("button");
-
-botoes[0].onclick = function () {
-  try {
-    index = index - 1;
-
-    if (receitas[index] == undefined) {
-      index = 0;
-    }
-
+document.getElementById("botaovoltar").onclick = function () {
+  if (index > 0) {
+    index--;
     mostrarReceita();
-
-  } catch (erro) {
-    console.log(erro);
   }
 };
 
-botoes[1].onclick = function () {
-  try {
-    index = index + 1;
-
-    if (receitas[index] == undefined) {
-      index = index - 1;
-    }
-
+document.getElementById("botaoavancar").onclick = function () {
+  if (index < receitas.length - 1) {
+    index++;
     mostrarReceita();
-
-  } catch (erro) {
-    console.log(erro);
   }
 };
 
-buscarReceitas();
+ buscarReceitas()
